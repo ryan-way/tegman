a762c0889413a9b0c31bb66c9aff7778148a24f9
ryan-way <ryan.keith.way@gmail.com>
2024-05-25 01:13:41 -0700

    closes #11: added apigateway with lambda integration

--- terraform/main.tf
+++ terraform/main.tf
@@ -53,7 +53,7 @@ data "archive_file" "temperature_lambda_archive_file" {
   output_path = "bootstrap.zip"
 }
 
-resource "aws_lambda_function" "tegman_lambda" {
+resource "aws_lambda_function" "temperature_lambda" {
   # If the file is not in the current working directory you will need to include a
   # path.module in the filename.
 
@@ -69,4 +69,60 @@ resource "aws_lambda_function" "tegman_lambda" {
       foo = "bar"
     }
   }
+}
+
+resource "aws_api_gateway_rest_api" "temperature_api" {
+  name = "temperature_api"
+}
+
+resource "aws_api_gateway_resource" "temperature_api_resource" {
+  path_part   = "resource"
+  parent_id   = aws_api_gateway_rest_api.temperature_api.root_resource_id
+  rest_api_id = aws_api_gateway_rest_api.temperature_api.id
+}
+
+resource "aws_api_gateway_method" "temperature_api_resource_post_method" {
+  rest_api_id   = aws_api_gateway_rest_api.temperature_api.id
+  resource_id   = aws_api_gateway_resource.temperature_api_resource.id
+  http_method   = "POST"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "integration" {
+  rest_api_id             = aws_api_gateway_rest_api.temperature_api.id
+  resource_id             = aws_api_gateway_resource.temperature_api_resource.id
+  http_method             = aws_api_gateway_method.temperature_api_resource_post_method.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.temperature_lambda.invoke_arn
+}
+
+# Lambda
+resource "aws_lambda_permission" "apigw_lambda_permission" {
+  statement_id  = "AllowExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.temperature_lambda.function_name
+  principal     = "apigateway.amazonaws.com"
+
+  # More: http://docs.aws.amazon.com/apigateway/latest/developerguide/api-gateway-control-access-using-iam-policies-to-invoke-api.html
+  source_arn = "arn:aws:execute-api:us-west-1:058264156666:${aws_api_gateway_rest_api.temperature_api.id}/*/${aws_api_gateway_method.temperature_api_resource_post_method.http_method}${aws_api_gateway_resource.temperature_api_resource.path}"
+}
+
+# IAM
+data "aws_iam_policy_document" "apigw_lambda_assume_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role" "role" {
+  name               = "myrole"
+  assume_role_policy = data.aws_iam_policy_document.apigw_lambda_assume_role.json
 }
