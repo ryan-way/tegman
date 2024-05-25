de691b9fecdb3d25e64479e8edce264ec841e268
ryan-way <ryan.keith.way@gmail.com>
2024-05-25 00:33:07 -0700

    closes #9: setup aurora database

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,3 +11,20 @@ provider "aws" {
   region  = "us-west-1"
   profile = "tegmen"
 }
+
+resource "aws_rds_cluster" "tegmen_db" {
+  cluster_identifier      = "tegmen"
+  engine                  = "aurora-postgresql"
+  engine_mode             = "serverless"
+  availability_zones      = ["us-west-1a", "us-west-1b", "us-west-1a"]
+  database_name           = "tegmen"
+  master_username         = "root"
+  master_password         = "rootroot"
+  backup_retention_period = 5
+  enable_http_endpoint    = true
+  preferred_backup_window = "07:00-09:00"
+  skip_final_snapshot     = true
+  scaling_configuration {
+    min_capacity = 2
+  }
+}
