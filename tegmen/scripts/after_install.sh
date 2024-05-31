e90f2b505bbb2fe940d43e6c194b0112b33b784a
ryan-way <ryan.keith.way@gmail.com>
2024-05-30 17:46:29 -0700

    Testing crontab update

--- tegmen/scripts/after_install.sh
+++ tegmen/scripts/after_install.sh
@@ -3,4 +3,5 @@ date >> $OUTPUT
 echo "install binary..." >> $OUTPUT
 /root/.cargo/bin/cargo install --path /root/tegmen >> $OUTPUT || exit 1
 echo "restarting cron..." >> $OUTPUT
+crontab /root/tegmen/crontab >> $OUTPUT || exit 1
 service cron restart >> $OUTPUT || exit 1
