e265da81388f7338192d5103ca2fba3f728550e6
ryan-way <ryan.keith.way@gmail.com>
2024-06-01 18:46:39 -0700

    Adding api gateway module

--- terraform/api-gateway/main.tf
+++ terraform/api-gateway/main.tf
@@ -3,44 +3,11 @@ variable "lambda_arn" { type = string }
 variable "lambda_invoke_arn" { type = string }
 variable "function_name" { type = string }
 
-resource "aws_api_gateway_rest_api" "api" {
-  name = var.name
-}
-
-resource "aws_api_gateway_resource" "api_resource" {
-  path_part   = "resource"
-  parent_id   = aws_api_gateway_rest_api.api.root_resource_id
-  rest_api_id = aws_api_gateway_rest_api.api.id
-}
-
-resource "aws_api_gateway_method" "api_resource_post_method" {
-  rest_api_id   = aws_api_gateway_rest_api.api.id
-  resource_id   = aws_api_gateway_resource.api_resource.id
-  http_method   = "POST"
-  authorization = "NONE"
-}
-
-resource "aws_api_gateway_integration" "integration" {
-  rest_api_id             = aws_api_gateway_rest_api.api.id
-  resource_id             = aws_api_gateway_resource.api_resource.id
-  http_method             = aws_api_gateway_method.api_resource_post_method.http_method
-  integration_http_method = "POST"
-  type                    = "AWS_PROXY"
-  uri                     = var.lambda_invoke_arn
-}
-
 data "aws_region" "current" {}
 data "aws_caller_identity" "current" {}
 
-# Lambda
-resource "aws_lambda_permission" "apigw_lambda_permission" {
-  statement_id  = "AllowExecutionFromAPIGateway"
-  action        = "lambda:InvokeFunction"
-  function_name = var.function_name
-  principal     = "apigateway.amazonaws.com"
-
-  # More: http://docs.aws.amazon.com/apigateway/latest/developerguide/api-gateway-control-access-using-iam-policies-to-invoke-api.html
-  source_arn = "arn:aws:execute-api:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:${aws_api_gateway_rest_api.api.id}/*/${aws_api_gateway_method.api_resource_post_method.http_method}${aws_api_gateway_resource.api_resource.path}"
+resource "aws_api_gateway_rest_api" "api" {
+  name = var.name
 }
 
 # IAM
@@ -61,3 +28,65 @@ resource "aws_iam_role" "api_role" {
   name               = "${var.name}_role"
   assume_role_policy = data.aws_iam_policy_document.api_lambda_assume_role_policy.json
 }
+
+resource "aws_api_gateway_resource" "temperature_resource" {
+  path_part   = "temperature"
+  parent_id   = aws_api_gateway_rest_api.api.root_resource_id
+  rest_api_id = aws_api_gateway_rest_api.api.id
+}
+
+resource "aws_api_gateway_method" "temperature_resource_post_method" {
+  rest_api_id   = aws_api_gateway_rest_api.api.id
+  resource_id   = aws_api_gateway_resource.temperature_resource.id
+  http_method   = "POST"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "temperature_resource_post_method_integration" {
+  rest_api_id             = aws_api_gateway_rest_api.api.id
+  resource_id             = aws_api_gateway_resource.temperature_resource.id
+  http_method             = aws_api_gateway_method.temperature_resource_post_method.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = var.lambda_invoke_arn
+}
+
+# Lambda
+resource "aws_lambda_permission" "apigw_post_lambda_permission" {
+  statement_id  = "AllowPostExecution"
+  action        = "lambda:InvokeFunction"
+  function_name = var.function_name
+  principal     = "apigateway.amazonaws.com"
+
+  # More: http://docs.aws.amazon.com/apigateway/latest/developerguide/api-gateway-control-access-using-iam-policies-to-invoke-api.html
+  source_arn = "arn:aws:execute-api:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:${aws_api_gateway_rest_api.api.id}/*/${aws_api_gateway_method.temperature_resource_post_method.http_method}${aws_api_gateway_resource.temperature_resource.path}"
+}
+
+
+resource "aws_api_gateway_method" "temperature_resource_get_method" {
+  rest_api_id   = aws_api_gateway_rest_api.api.id
+  resource_id   = aws_api_gateway_resource.temperature_resource.id
+  http_method   = "GET"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "temperature_resource_get_method_integration" {
+  rest_api_id             = aws_api_gateway_rest_api.api.id
+  resource_id             = aws_api_gateway_resource.temperature_resource.id
+  http_method             = aws_api_gateway_method.temperature_resource_get_method.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = var.lambda_invoke_arn
+}
+
+# Lambda
+resource "aws_lambda_permission" "apigw_get_lambda_permission" {
+  statement_id  = "AllowGetExecution"
+  action        = "lambda:InvokeFunction"
+  function_name = var.function_name
+  principal     = "apigateway.amazonaws.com"
+
+  # More: http://docs.aws.amazon.com/apigateway/latest/developerguide/api-gateway-control-access-using-iam-policies-to-invoke-api.html
+  source_arn = "arn:aws:execute-api:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:${aws_api_gateway_rest_api.api.id}/*/${aws_api_gateway_method.temperature_resource_get_method.http_method}${aws_api_gateway_resource.temperature_resource.path}"
+}
+
