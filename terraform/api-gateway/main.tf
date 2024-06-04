399d5ed3b513eaa76849b6d401573d412a2da782
ryan-way <ryan.keith.way@gmail.com>
2024-06-03 23:04:32 -0700

    Removed get endpoint in favor of single post entry point

--- terraform/api-gateway/main.tf
+++ terraform/api-gateway/main.tf
@@ -62,31 +62,3 @@ resource "aws_lambda_permission" "apigw_post_lambda_permission" {
   source_arn = "arn:aws:execute-api:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:${aws_api_gateway_rest_api.api.id}/*/${aws_api_gateway_method.temperature_resource_post_method.http_method}${aws_api_gateway_resource.temperature_resource.path}"
 }
 
-
-resource "aws_api_gateway_method" "temperature_resource_get_method" {
-  rest_api_id   = aws_api_gateway_rest_api.api.id
-  resource_id   = aws_api_gateway_resource.temperature_resource.id
-  http_method   = "GET"
-  authorization = "NONE"
-}
-
-resource "aws_api_gateway_integration" "temperature_resource_get_method_integration" {
-  rest_api_id             = aws_api_gateway_rest_api.api.id
-  resource_id             = aws_api_gateway_resource.temperature_resource.id
-  http_method             = aws_api_gateway_method.temperature_resource_get_method.http_method
-  integration_http_method = "POST"
-  type                    = "AWS_PROXY"
-  uri                     = var.lambda_invoke_arn
-}
-
-# Lambda
-resource "aws_lambda_permission" "apigw_get_lambda_permission" {
-  statement_id  = "AllowGetExecution"
-  action        = "lambda:InvokeFunction"
-  function_name = var.function_name
-  principal     = "apigateway.amazonaws.com"
-
-  # More: http://docs.aws.amazon.com/apigateway/latest/developerguide/api-gateway-control-access-using-iam-policies-to-invoke-api.html
-  source_arn = "arn:aws:execute-api:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:${aws_api_gateway_rest_api.api.id}/*/${aws_api_gateway_method.temperature_resource_get_method.http_method}${aws_api_gateway_resource.temperature_resource.path}"
-}
-
