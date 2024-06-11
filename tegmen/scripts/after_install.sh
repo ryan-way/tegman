a4354d0a7565db8faa088e4c0507391277933db7
ryan-way <ryan.keith.way@gmail.com>
2024-06-11 15:17:18 -0700

    Fixing deployment paths to include local dependencies

--- tegmen/scripts/after_install.sh
+++ tegmen/scripts/after_install.sh
@@ -1,7 +1,7 @@
 OUTPUT=/var/log/code_deploy_scripts/$(date +%F-%H).log
 date >> $OUTPUT
 echo "install binary..." >> $OUTPUT
-/root/.cargo/bin/cargo install --path /root/tegmen >> $OUTPUT || exit 1
+/root/.cargo/bin/cargo install --path /root/tegmen/tegmen >> $OUTPUT || exit 1
 echo "restarting cron..." >> $OUTPUT
 crontab /root/tegmen/crontab >> $OUTPUT || exit 1
 service cron restart >> $OUTPUT || exit 1
