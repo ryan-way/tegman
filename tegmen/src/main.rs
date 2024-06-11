56b0fb305b784ecac78015e1ec5ec748060bd3b0
ryan-way <ryan.keith.way@gmail.com>
2024-06-11 16:24:26 -0700

    Add cloudwatch metrics for tegmen

--- tegmen/src/main.rs
+++ tegmen/src/main.rs
@@ -1,5 +1,6 @@
 extern crate dht22_pi;
 mod client;
+use aws_sdk_cloudwatch::types::{Dimension, MetricDatum};
 use client::Client;
 use contract::request::LogTemperature;
 use contract::Client as Contract;
@@ -18,15 +19,56 @@ async fn main() {
             return;
         }
     };
-    let client = Client::new();
     let log_temperature = LogTemperature {
         temperature: convert_to_fahrenheit(temperature_reading.temperature),
         humidity: temperature_reading.humidity,
         host_name: hostname::get().unwrap().to_str().unwrap().to_owned(),
     };
+
     println!("Temperature reading: {:?}", log_temperature);
 
-    client.log_temperature(log_temperature).await.unwrap();
+    let config = aws_config::load_from_env().await;
+    let client = aws_sdk_cloudwatch::Client::new(&config);
+    let datum = MetricDatum::builder()
+        .metric_name("Temperature")
+        .dimensions(
+            Dimension::builder()
+                .name("Host Name")
+                .value(&log_temperature.host_name)
+                .build(),
+        )
+        .value(log_temperature.temperature as f64)
+        .build();
 
+    client
+        .put_metric_data()
+        .namespace("tegmen")
+        .metric_data(datum)
+        .send()
+        .await
+        .unwrap();
+
+    let datum = MetricDatum::builder()
+        .metric_name("Humidity")
+        .dimensions(
+            Dimension::builder()
+                .name("Host Name")
+                .value(&log_temperature.host_name)
+                .build(),
+        )
+        .value(log_temperature.humidity as f64)
+        .build();
+
+    client
+        .put_metric_data()
+        .namespace("tegmen")
+        .metric_data(datum)
+        .send()
+        .await
+        .unwrap();
+
+    println!("Metrics logged");
+    let client = Client::new();
+    client.log_temperature(log_temperature).await.unwrap();
     println!("Temperature send to api");
 }
